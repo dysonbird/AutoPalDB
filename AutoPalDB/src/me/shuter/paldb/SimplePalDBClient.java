ef5fd0e469927a5338521c2ee26d338bd6954652
dysonbird <dodo98@126.com>
2015-12-04 16:00:43 +0800

    simple client

---
+++ AutoPalDB/src/me/shuter/paldb/SimplePalDBClient.java
@@ -0,0 +1,23 @@
+package me.shuter.paldb;
+
+import java.io.File;
+
+import com.linkedin.paldb.api.Configuration;
+import com.linkedin.paldb.api.PalDB;
+import com.linkedin.paldb.api.StoreReader;
+
+import me.shuter.paldb.entity.config.Config;
+
+public class SimplePalDBClient {
+	private StoreReader reader;
+	
+	public SimplePalDBClient(String db) {
+		Configuration config = Config.getConfigure();
+		reader = PalDB.createReader(new File(db), config);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T> T getValue(String key, Class<T> t) {
+		return (T)reader.get(key);
+	}
+}
